9f8426776f0ddd4d04f05fa185ca365ada81aa35
Daniel Gading <daniel.gading@agencychief.com>
2018-01-25 16:49:39 -0500

    Add initial validation for images and multi select creation

--- src/Form/NaraImageAdd.php
+++ src/Form/NaraImageAdd.php
@@ -4,8 +4,6 @@ namespace Drupal\nara_image_tool\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\RequestException;
 use Drupal\Core\StringTranslation\TranslationInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\media\Entity\Media;
@@ -32,6 +30,7 @@ class NaraImageAdd extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $nara_items = $form_state->get('nara_items');
 
     $form['naid'] = [
       '#type' => 'number',
@@ -40,14 +39,70 @@ class NaraImageAdd extends FormBase {
       '#required' => TRUE,
     ];
 
-    $form['actions'] = [
-      '#type' => 'actions',
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#submit' => ['::getArchiveItems'],
+      '#ajax' => [
+        'callback' => '::returnArchiveItems',
+        'wrapper' => 'media-gallery-wrapper',
+      ],
+      '#value' => $this->t('Find Archive Item'),
     ];
 
-    $form['actions']['submit'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Submit'),
+    $form['media_gallery'] = [
+      '#type' => 'container',
+      '#prefix' => '<div id="media-gallery-wrapper">',
+      '#suffix' => '</div>',
+      '#tree' => TRUE,
     ];
+
+    for ($i = 0; $i < count($form_state->get('nara_items')); $i++) {
+      $item = $form_state->get('nara_items')[$i];
+      $form['media_gallery'][$i] = [
+        '#type' => 'fieldset',
+        '#title' => $item->file->{'@name'},
+      ];
+
+      $form['media_gallery'][$i]['addMedia'] = [
+        '#type' => 'checkbox',
+        '#title' => '<img src="' . $item->thumbnail->{'@url'} . '" alt="' . $item->file->{'@name'} . ' thumbnail" />',
+        '#suffix' => '<a href="' . $item->file->{'@url'} . '" target="_blank" rel="noopener noreferrer">Open image in new window</a>',
+      ];
+
+      $form['media_gallery'][$i]['media_title'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Media Title'),
+        '#default_value' => $item->file->{'@name'},
+        '#description' => $this->t('Appears when searching for Media entities in Drupal.'),
+        '#required' => TRUE,
+      ];
+
+      $form['media_gallery'][$i]['image_title'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Image Title'),
+        '#description' => $this->t('Only needed if different than Media title. This appears when hovering over an image in the browser.'),
+        '#required' => FALSE,
+      ];
+
+      $form['media_gallery'][$i]['alt_text'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Image Alt Text'),
+        '#description' => $this->t('Alt image required for accessibility.'),
+        '#required' => FALSE,
+      ];
+
+    }
+
+    if (count($form_state->get('nara_items')) > 0) {
+      $form['media_gallery']['actions'] = [
+        '#type' => 'actions',
+      ];
+
+      $form['media_gallery']['actions']['submit'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Create Media'),
+      ];
+    }
 
     return $form;
   }
@@ -56,31 +111,71 @@ class NaraImageAdd extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
+    return NULL;
+  }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function returnArchiveItems(array &$form, FormStateInterface $form_state) {
+    return $form['media_gallery'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getArchiveItems(array &$form, FormStateInterface $form_state) {
+    $client = \Drupal::httpClient();
+    $naId = $form_state->getValue('naid');
+    $_nara_items = $form_state->get('nara_items');
+    $res = json_decode($client->request('GET', 'https://catalog.archives.gov/api/v1?naIds=' . $naId)->getBody()->getContents());
+    $nara_object;
+    if ($res->opaResponse->results->result[0]->objects->object == NULL) {
+      drupal_set_message(t('Incorrect ID or no object found at ID.'), 'error', TRUE);
+      return $form['media_gallery'];
+    }
+    else {
+      $nara_object = $res->opaResponse->results->result[0]->objects->object;
+      if (is_array($nara_object)) {
+        foreach ($nara_object as $key => $value) {
+          if ($value->file->{'@mime'} === 'image/jpeg') {
+            $_nara_items[] = $value;
+          }
+        }
+      }
+      else {
+        $_nara_items[] = $nara_object;
+      }
+    }
+    $form_state->set('nara_items', $_nara_items);
+    $form_state->setRebuild();
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-
-    $client = new \GuzzleHttp\Client();
-    $naId = $form_state->getValue('naid');
-    $res = json_decode($client->get('https://catalog.archives.gov/api/v1?naIds=' . $naId)->getBody()->getContents());
-    $url = $res->opaResponse->results->result[0]->objects->object->file->{'@url'};
-    $data = system_retrieve_file($url, NULL, TRUE, FILE_EXISTS_REPLACE);
-
-    $image_media = Media::create([
-      'bundle' => 'image',
-      'uid' => \Drupal::currentUser()->id(),
-      'langcode' => \Drupal::languageManager()->getDefaultLanguage()->getId(),
-      'field_media_image' => [
-        'target_id' => $data->id(),
-        'alt' => t('Placeholder image'),
-        'title' => t('Placeholder image'),
-      ],
-    ]);
-    $image_media->save();
+    $new_media = 0;
+    for ($i = 0; $i < count($form_state->get('nara_items')); $i++) {
+      $item = $form_state->get('nara_items')[$i];
+      if ($form_state->getValue(['media_gallery', (string) $i, 'addMedia']) == 1) {
+        $new_media++;
+        $data = system_retrieve_file($item->file->{'@url'}, NULL, TRUE, FILE_EXISTS_REPLACE);
+        $image_media = Media::create([
+          'bundle' => 'image',
+          'uid' => \Drupal::currentUser()->id(),
+          'langcode' => \Drupal::languageManager()->getDefaultLanguage()->getId(),
+          'name' => $form_state->getValue(['media_gallery', (string) $i, 'media_title']),
+          'field_media_image' => [
+            'target_id' => $data->id(),
+            'alt' => $form_state->getValue(['media_gallery', (string) $i, 'alt_text']),
+            'title' => ($form_state->getValue(['media_gallery', (string) $i, 'image_title'])) ?: $form_state->getValue(['media_gallery', (string) $i, 'media_title']),
+          ],
+        ]);
+        $image_media->save();
+      }
+    }
+    drupal_set_message(t('@media new Media added.', ['@media' => $new_media]), 'status');
   }
 
 }
